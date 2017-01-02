d76c90d979d54a0980b84090b2c8f5fcbffd5237
YanyingWang <yanyingwang1@gmail.com>
2017-01-02 12:05:07 +0800

    fix mina deloyment done restart error

--- config/deploy.rb
+++ config/deploy.rb
@@ -99,9 +99,9 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     to :launch do
-      #queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-      #queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
-      invoke 'puma:phased_restart'
+      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      #invoke 'puma:phased_restart'
     end
   end
 end
